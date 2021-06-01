eb0fa0fe5623ac52b95319f73fa9adbc4254c459
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-01 15:14:36 +0530

    Added install_requires to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,13 @@ DIST_NAME = 'utils'
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-VERSION = 2
-SUB_VERSION = 0
+VERSION = 0
+SUB_VERSION = 2
+SUB_SUB_VERSION = 1
 
 setuptools.setup(
     name="%s-nuuuwan" % DIST_NAME,
-    version="0.%d.%d" % (VERSION, SUB_VERSION),
+    version="%d.%d.%d" % (VERSION, SUB_VERSION, SUB_SUB_VERSION),
     author="Nuwan I. Senaratna",
     author_email="nuuuwan@gmail.com",
     description="Simple extensions to the core python libraries.",
@@ -28,6 +29,12 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+
+    install_requires=[
+        'requests',
+        'psutil',
+        'area',
+    ],
     test_suite='nose.collector',
     tests_require=['nose'],
 )
