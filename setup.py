84b7a8e0ad49d0761d7bfc73ab32d11d4cd305d2
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-07-08 14:10:58 +0530

    PRE-RELEASE 1.0.21

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ long_description = ''
 with open('README.md') as fin:
     long_description = fin.read()
 
-IS_PRE_RELEASE = False
-MAJOR, MINOR, PATCH = 1, 0, 20
+IS_PRE_RELEASE = True
+MAJOR, MINOR, PATCH = 1, 0, 21
 
 if IS_PRE_RELEASE:
     ts = time.strftime('%Y%m%d%H%M%S0000', time.localtime())
--- src/utils/ds.py
+++ src/utils/ds.py
@@ -4,30 +4,6 @@
 def dict_list_to_index(dict_list, key):
     """Given a list of dicts, returns an index mapping each dict item
     to the dict.
-
-    Args:
-        dict_list (list of dicts): list of dicts
-        key (str): key
-
-    Return:
-        index (dict)
-
-    .. code-block:: python
-
-        >>> from utils.ds import dict_list_to_index
-        >>> dict_list = [
-            {'name': 'Alice', 'age': 20},
-            {'name': 'Bob', 'age': 25},
-            {'name': 'Charlie', 'age': 55},
-        ]
-        >>> print(dict_list_to_index(dict_list, 'name'))
-        {
-            'Alice': {'name': 'Alice', 'age': 20},
-            'Bob': {'name': 'Bob', 'age': 25},
-            'Charlie': {'name': 'Charlie', 'age': 55},
-        }
-
-
     """
     return dict(
         zip(
@@ -43,40 +19,12 @@ def dict_list_to_index(dict_list, key):
 
 
 def unique(lst):
-    """Get unique values from list.
-
-    Args:
-        lst (list): List
-
-    Return:
-        list of unique values
-
-    .. code-block:: python
-
-        >>> from utils.ds import unique
-        >>> lst = [1, 1, 1, 2]
-        >>> print(unique(lst))
-        [1, 2]
-
-    """
+    """Get unique values from list."""
     return list(set(lst))
 
 
 def flatten(list_of_list):
-    """Flatten list of lists.
-
-    Args:
-        list_of_list(list): list of lists
-
-    Return:
-        flattened list
-
-    .. code-block:: python
-
-    >>> print(flatten([[1, 2], [3, 4, 5], [6], []]))
-    [1, 2, 3, 4, 5, 6]
-
-    """
+    """Flatten list of lists."""
     flattened_list = []
     for lst in list_of_list:
         flattened_list += lst
@@ -89,3 +37,15 @@ def sort_dict_items_by_key(_dict):
         _dict.items(),
         key=lambda item: item[0],
     )
+
+
+def dict_get(_dict, keys):
+    """Get dict values by keys."""
+    return [_dict[key] for key in keys]
+
+
+def dict_set(_dict, keys, values):
+    """Set dict values by keys."""
+    for key, value in zip(keys, values):
+        _dict[key] = value
+    return _dict
