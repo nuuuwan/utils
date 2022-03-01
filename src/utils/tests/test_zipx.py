07c57d7e9ef14492101d3fe49543ba83d4a96e0d
nuuuwan <nuuuwan@gmail.com>
2022-03-01 14:33:22 +0530

    Fixed bug in test_zipx

--- src/utils/tests/test_zipx.py
+++ src/utils/tests/test_zipx.py
@@ -10,20 +10,19 @@ class TestZipX(unittest.TestCase):
     """Test."""
 
     def test_download_and_unzip(self):
+        os.system('rm -rf /tmp/utils.test_zipx*')
+
         url_download = os.path.join(
-            'https://github.com',
-            'nuuuwan/utils',
-            'blob/main/src/utils/tests/test_zipx_example1.txt.zip?raw=true',
+            'https://raw.githubusercontent.com',
+            'nuuuwan/utils/main',
+            'src/utils/tests/test_zipx_example1.txt.zip ',
         )
-        zip_file = '/tmp/utils.test_zipx_example1_actual.txt.zip'
+        zip_file = '/tmp/utils.test_zipx_example1.zip'
         download_zip(url_download, zip_file)
 
-        actual_txt_file = os.path.join(
-            '/tmp/utils.test_zipx_example1_actual.txt',
-            'test_zipx_example1.txt'
-        )
-        unzip(zip_file, actual_txt_file)
-
+        dir_unzip = os.path.join('/tmp/test_zipx_example1')
+        unzip(zip_file, dir_unzip)
+        actual_txt_file = os.path.join(dir_unzip, 'test_zipx_example1.txt')
         expected_txt_file = 'src/utils/tests/test_zipx_example1.txt'
 
         self.assertListEqual(
