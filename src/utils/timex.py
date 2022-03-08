6d56b255acd999516c3adb037946440bf1fea121
nuuuwan <nuuuwan@gmail.com>
2022-03-08 17:17:35 +0530

    Added timezone tests

--- src/utils/timex.py
+++ src/utils/timex.py
@@ -20,6 +20,12 @@ class SECONDS_IN:
     YEAR = 365.25 * 86400
 
 
+def get_timezone_offset(timezone=None):
+    if timezone is None:
+        return 0
+    return timezone - time.timezone
+
+
 def get_timezone():
     """Get timezone."""
     return str(datetime.datetime.now().astimezone().tzinfo)
@@ -33,18 +39,23 @@ def get_unixtime():
 def parse_time(
     time_str,
     time_format=DEFAULT_TIME_FORMAT,
+    timezone=None,
 ):
     """Parse time string, and return unixtime."""
     _datetime = datetime.datetime.strptime(time_str, time_format)
-    return (int)(time.mktime(_datetime.timetuple()))
+    return (int)(time.mktime(_datetime.timetuple())) \
+        + get_timezone_offset(timezone)
 
 
 def format_time(
     unixtime,
     time_format=DEFAULT_TIME_FORMAT,
+    timezone=None,
 ):
     """Format unixtime as time string."""
-    _datetime = datetime.datetime.fromtimestamp(unixtime)
+    _datetime = datetime.datetime.fromtimestamp(
+        unixtime - get_timezone_offset(timezone),
+    )
     return _datetime.strftime(time_format)
 
 
@@ -55,21 +66,19 @@ def format_current_date_with_timezone():
     return '{date} {timezone}'.format(date=date, timezone=timezone)
 
 
-def get_date_id(unixtime=None):
+def get_date_id(unixtime=None, timezone=None,):
     """Get date id."""
     if unixtime is None:
         unixtime = get_unixtime()
-    return format_time(unixtime, FORMAT_DATE_ID)
+    return format_time(unixtime, FORMAT_DATE_ID, timezone)
 
 
-def parse_date_id(date_id):
-    return parse_time(date_id, FORMAT_DATE_ID)
+def parse_date_id(date_id, timezone=None):
+    return parse_time(date_id, FORMAT_DATE_ID, timezone)
 
 
-def get_date(unixtime=None):
+def get_date(unixtime=None, timezone=None):
     """Get date id."""
     if unixtime is None:
         unixtime = get_unixtime()
-    return format_time(unixtime, FORMAT_DATE)
-
-
+    return format_time(unixtime, FORMAT_DATE, timezone)
