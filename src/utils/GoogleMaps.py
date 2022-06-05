0a39899ccd4979bdab167949fd5d0e48d68288f3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-05 09:45:11 +0530

    Added implementation; Tests Pass

--- src/utils/GoogleMaps.py
+++ src/utils/GoogleMaps.py
@@ -1,9 +1,22 @@
+import os
+
+import googlemaps
+
+
+def get_api_key():
+    return os.environ['GOOGLE_API_KEY']
+
+
 class GoogleMaps:
     def __init__(self):
-        pass
+        gmaps_api_key = get_api_key()
+        self.api = googlemaps.Client(key=gmaps_api_key)
 
     def get_latlng(self, address):
-        pass
+        geocode = self.api.geocode(address)
+        location = geocode[0]['geometry']['location']
+        return [location['lat'], location['lng']]
 
     def get_address(self, latlng):
-        pass
+        rev_geocode = self.api.reverse_geocode(latlng)
+        return rev_geocode[0]['formatted_address']
