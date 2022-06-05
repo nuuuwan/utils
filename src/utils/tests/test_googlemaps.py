97153363b26af97eb2521ec6c2963ebe0833f5f7
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-05 09:54:17 +0530

    Added more tests; Fixed precision error

--- src/utils/tests/test_googlemaps.py
+++ src/utils/tests/test_googlemaps.py
@@ -8,6 +8,14 @@ TEST_ADDRESS_AND_LATLNG = [
         'WV87+7CW, Dr CWW Kannangara Mawatha, Colombo 00700, Sri Lanka',
         [6.9157375, 79.8635781],
     ],
+    [
+        '101 Kumaratunga Munidasa Mawatha, Colombo 00700, Sri Lanka',
+        [6.8994025, 79.8600437],
+    ],
+    [
+        '54 Chatham St, Colombo 00100, Sri Lanka',
+        [6.9344566, 79.8430699],
+    ]
 ]
 
 
@@ -22,7 +30,8 @@ class TestCase(unittest.TestCase):
         google_maps = GoogleMaps()
         for address, expected_latlng in TEST_ADDRESS_AND_LATLNG:
             actual_latlng = google_maps.get_latlng(address)
-            self.assertEqual(expected_latlng, actual_latlng)
+            self.assertAlmostEqual(expected_latlng[0], actual_latlng[0])
+            self.assertAlmostEqual(expected_latlng[1], actual_latlng[1])
 
 
 if __name__ == '__main__':
