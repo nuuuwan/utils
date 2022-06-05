a4bc398a2bd7dc162b7546fdacb9abbc1f5ac8e3
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-06-05 09:55:07 +0530

    Renamed private methods

--- src/utils/GoogleMaps.py
+++ src/utils/GoogleMaps.py
@@ -17,23 +17,23 @@ class GoogleMaps:
         gmaps_api_key = get_api_key()
         self.api = googlemaps.Client(key=gmaps_api_key)
 
-    def get_geocode(self, address):
+    def __get_geocode__(self, address):
         @cache(CACHE_NAME, CACHE_TIMEOUT)
         def get_geocode_inner(address):
             return self.api.geocode(address)
         return get_geocode_inner(address)
 
-    def get_reverse_geocode(self, latlng):
+    def __get_reverse_geocode__(self, latlng):
         @cache(CACHE_NAME, CACHE_TIMEOUT)
         def get_reverse_geocode_inner(latlng):
             return self.api.reverse_geocode(latlng)
         return get_reverse_geocode_inner(latlng)
 
     def get_latlng(self, address):
-        geocode = self.get_geocode(address)
+        geocode = self.__get_geocode__(address)
         location = geocode[0]['geometry']['location']
         return [location['lat'], location['lng']]
 
     def get_address(self, latlng):
-        rev_geocode = self.get_reverse_geocode(latlng)
+        rev_geocode = self.__get_reverse_geocode__(latlng)
         return rev_geocode[0]['formatted_address']
