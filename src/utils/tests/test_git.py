773112f9f3a0973a7e77b9c77200335ba6269a28
Nuwan I. Senaratna <nuuuwan@gmail.com>
2022-07-12 12:35:34 +0530

    Added force to unittest

--- src/utils/tests/test_git.py
+++ src/utils/tests/test_git.py
@@ -14,10 +14,10 @@ class TestCase(unittest.TestCase):
 
     def test_clone(self):
         git = Git(TEST_REPO_URL)
-        git.clone(TEST_DIR_REPO)
+        git.clone(TEST_DIR_REPO, force=True)
         git.checkout(TEST_BRACH_NAME)
         self.assertTrue(os.path.exists(TEST_DIR_REPO))
-        git.checkout('main')
+        
 
 
 if __name__ == '__main__':
