55011e3010444a2394e22d3cc06f6c6b6f5c2659
nuuuwan <nuuuwan@gmail.com>
2023-01-13 17:31:36 +0530

    refac: www -> WWW

--- src/utils/tests/___test_www.py
+++ src/utils/tests/___test_www.py
@@ -4,7 +4,7 @@ import unittest
 
 import pytest
 
-from utils import www
+from utils import WWW
 
 TEST_JSON_URL = os.path.join(
     'https://raw.githubusercontent.com',
@@ -29,31 +29,31 @@ class testWWW(unittest.TestCase):
     @pytest.mark.slow
     def test_read(self):
         """Test."""
-        data = www.read(TEST_JSON_URL)
+        data = WWW.read(TEST_JSON_URL)
         self.assertIn('Station', data)
-        data_selenium = www.read(TEST_JSON_URL, use_selenium=True)
+        data_selenium = WWW.read(TEST_JSON_URL, use_selenium=True)
         self.assertIn(data, data_selenium)
 
     def test_read_json(self):
         """Test."""
-        data = www.read_json(TEST_JSON_URL)
+        data = WWW.read_json(TEST_JSON_URL)
         self.assertIn('Station', data[0])
 
     def test_read_tsv(self):
         """Test."""
-        data = www.read_tsv(TEST_TSV_URL)
+        data = WWW.read_tsv(TEST_TSV_URL)
         self.assertEqual(len(data), 9)
         self.assertEqual(data[0]['province_id'], 'LK-1')
 
     def test_invalid_url(self):
         """Test."""
-        data = www.read_json(TEST_INVALID_URL)
+        data = WWW.read_json(TEST_INVALID_URL)
         self.assertEqual(data, None)
 
     def test_download_binary(self):
         """Test."""
         file_name = '/tmp/utils.test_www.file.png'
-        www.download_binary(
+        WWW.download_binary(
             TEST_IMAGE_LINK,
             file_name,
         )
@@ -61,12 +61,12 @@ class testWWW(unittest.TestCase):
     @pytest.mark.slow
     def test_exists(self):
         """Test."""
-        self.assertTrue(www.exists('https://www.python.org/'))
-        self.assertFalse(www.exists('https://www.python123.org/'))
+        self.assertTrue(WWW.exists('https://www.python.org/'))
+        self.assertFalse(WWW.exists('https://www.python123.org/'))
 
     def test_get_all_urls(self):
         """Test."""
         self.assertGreater(
-            len(www.get_all_urls('https://www.python.org/')),
+            len(WWW.get_all_urls('https://www.python.org/')),
             50,
         )
