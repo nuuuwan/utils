a822e2071752725b6c0c32b38d9e133b83c7280f
nuuuwan <nuuuwan@gmail.com>
2023-01-28 11:59:58 +0530

    refac: simplified test_log_metric

--- src/utils/tests/test_xmlx.py
+++ src/utils/tests/test_xmlx.py
@@ -4,6 +4,25 @@ import unittest
 
 from utils.xmlx import _, style
 
+TEST_BODY = _(
+    'body',
+    [
+        _('h1', 'This is header 1', style(font_family='Georgia')),
+        _('p', 'This is a paragraph'),
+        _(
+            'div',
+            [
+                _('span', 'This is a span in a div'),
+                _(
+                    'a',
+                    'This is a a(link) in a div',
+                    dict(href='https://pypi.org/project/utils-nuuuwan'),
+                ),
+            ],
+        ),
+    ],
+)
+
 
 class TestXMLX(unittest.TestCase):
     """Test."""
@@ -11,26 +30,7 @@ class TestXMLX(unittest.TestCase):
     def test_log_metric(self):
         """Test."""
         head = _('head')
-        body = _(
-            'body',
-            [
-                _('h1', 'This is header 1', style(font_family='Georgia')),
-                _('p', 'This is a paragraph'),
-                _(
-                    'div',
-                    [
-                        _('span', 'This is a span in a div'),
-                        _(
-                            'a',
-                            'This is a a(link) in a div',
-                            dict(
-                                href='https://pypi.org/project/utils-nuuuwan'
-                            ),
-                        ),
-                    ],
-                ),
-            ],
-        )
+        body = TEST_BODY
         html = _('html', [head, body])
         actual_file = '/tmp/utils.tests.test_xmlx.html'
         html.store(actual_file)
