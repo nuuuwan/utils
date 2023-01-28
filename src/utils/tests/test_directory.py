014ad1786deb80fdee111a9898b0ec51a8aa8277
nuuuwan <nuuuwan@gmail.com>
2023-01-28 17:23:06 +0530

    fix: unittest fails

--- src/utils/tests/test_directory.py
+++ src/utils/tests/test_directory.py
@@ -9,6 +9,6 @@ class TestDirectory(TestCase):
         self.assertEqual(dir_tests.path, 'src/utils/tests')
         self.assertEqual(dir_tests.name, 'tests')
         self.assertEqual(
-            dir_tests.children[0], File('src/utils/tests/data.csv')
+            dir_tests.children[0], File('src/utils/tests/banner.png')
         )
         self.assertTrue(dir_tests == Directory('src/utils/tests'))
