6d04364e9935e5ed9d2e8700387357ee9bf74d6b
nuuuwan <nuuuwan@gmail.com>
2023-01-28 22:24:25 +0530

    python-version: 3.8

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         'area',
         'bs4',
