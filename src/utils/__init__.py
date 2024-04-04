795d21650aa9d32801a852a43af7e98f16d9fc1d
nuuuwan <nuuuwan@gmail.com>
2024-04-04 10:01:53 +0530

    Replaced modules with wildcards

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,11 +1,9 @@
+# no-auto
 
-from utils_ai import AI
-from utils_base import (COLOR_BACKGROUND, COLOR_FOREGROUND, COLOR_FORMAT, TimeZoneOffset, Console, CSVFile,
-                        Dict, Directory, File, FiledVariable, Image, JSONFile,
-                        LatLng, LatLngLK, List, Log, PDFFile, String, Time,
-                        TimeDelta, TimeFormat, TimeUnit, TSVFile, XSVFile, Zip,
-                        _, Hash, Parallel, XMLElement, XMLUtils)
-from utils_git import Git
-from utils_lang import TTS, Language, Translator
-from utils_twitter import Tweet, Twitter
-from utils_www import WWW
+from utils_ai import *
+from utils_base import *
+from utils_base import _
+from utils_git import *
+from utils_lang import *
+from utils_twitter import *
+from utils_www import *
